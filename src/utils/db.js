38ea2aa129a24d35581d999b2c482483b769d2ef
dvasconcelos <di0g0vasconcelos@hotmail.com>
2024-11-05 01:11:05 -0300

    feat: connection to database and data

---
+++ src/utils/db.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2/promise')
+require('dotenv').config();
+
+const dbConfig = {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_ROOT_PASSWORD,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+}
+
+const dbConnection = async () => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    console.log('Connection to the database successfully established!');
+    return connection;
+  } catch (error) {
+    console.error('Error connecting to database:', error.message);
+    throw error;
+  }
+}
+
+module.exports = dbConnection;
