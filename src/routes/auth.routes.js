0ac63827f7e4faa4b5d8c72110f62a834739b7a8
dvasconcelos <di0g0vasconcelos@hotmail.com>
2024-11-07 02:44:37 -0300

    feat: router create user

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const UserController = require('../controllers/user.controller')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 const router = express.Router()
 
---
+++ src/services/user.service.js
@@ -0,0 +1,14 @@
+const UserModel = require('../models/user.model');
+const UserException = require('../errors/user.exception');
+
+const userService = {
+  async create(user) {
+    try {
+      return await UserModel.register(user);
+    } catch (error) {
+      console.error('Error creating user:', error.message);
+      throw new UserException('Internal Server Error', 500, 'INTERNAL_SERVER_ERROR');
+    }
+  }
+}
+module.exports = userService;
