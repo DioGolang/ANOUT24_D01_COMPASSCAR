1771e24ff52a34537a6c842a9a2f07ff7733bfd4
dvasconcelos <di0g0vasconcelos@hotmail.com>
2024-11-07 04:31:23 -0300

    fix: error controller user

--- src/presentation/routes/auth.routes.js
+++ src/presentation/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const AuthController = require('../controllers/')
+const AuthController = require('../controllers/auth.controller')
 
 const router = express.Router()
+
+router.post('/register', AuthController.register)
 
 module.exports = router;
