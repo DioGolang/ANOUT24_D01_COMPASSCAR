a33b0508525b1734b2096df829c4ba78daafb62d
dvasconcelos <di0g0vasconcelos@hotmail.com>
2024-11-11 16:56:28 -0300

    fix: update items

--- src/application/middlewares/car-update.handler.js
+++ src/application/middlewares/car-update.handler.js
@@ -2,35 +2,33 @@ const CarException = require('../../domain/exceptions/Car.exception');
 const { validatePlate, validateYear } = require('../../validators/car.validator');
 
 const carUpdateHandler = async (req, res, next) => {
-  try {
-    const { plate, year, model, brand } = req.body;
-    const errors = [];
+	try {
+		const { plate, year, model, brand } = req.body;
+		const errors = [];
 
-    console.log('plate', plate);
-    console.log('year', year);
-    console.log('model', model);
-    console.log('brand', brand);
+		console.log('plate', plate);
+		console.log('year', year);
+		console.log('model', model);
+		console.log('brand', brand);
 
+		if (plate !== undefined && !validatePlate(plate)) {
+			errors.push('plate must be in the correct format ABC-1C34');
+		}
 
-    if (plate !== undefined && !validatePlate(plate)) {
-      errors.push('plate must be in the correct format ABC-1C34');
-    }
+		if (year !== undefined && !validateYear(year)) {
+			errors.push(`year must be between ${new Date().getFullYear() - 10} and ${new Date().getFullYear() + 1}`);
+			console.log(errors);
+		}
 
-    if (year !== undefined && !validateYear(year)) {
-      errors.push(`year must be between ${new Date().getFullYear() - 10} and ${new Date().getFullYear()}`);
-      console.log(errors);
-    }
+		if (brand !== undefined && model === undefined) {
+			errors.push('model must also be informed');
+		}
 
-    if(brand !== undefined && model === undefined) {
-      errors.push('model must also be informed');
-    }
-
-    if (errors.length > 0) {
-      throw new CarException(errors.join(', '), 400, 'VALIDATION_ERROR');
-    }
-
-  } catch (error) {
-    next(error);
-  }
-}
+		if (errors.length > 0) {
+			throw new CarException(errors.join(', '), 400, 'VALIDATION_ERROR');
+		}
+	} catch (error) {
+		next(error);
+	}
+};
 module.exports = carUpdateHandler;
